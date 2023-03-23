144474c239c152197bc4f0d97b55b36728733c96
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-03-23 11:38:08 -0300

    feat: tela-de-animacao: adicionado textos;

--- AppOxxo/App.js
+++ AppOxxo/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, Image, View, Text } from 'react-native';
 import icon from './assets/oxxo_logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={icon}/>
+      <Text>Oxxo</Text>
+      <Text>Mercado</Text>
       <StatusBar style="auto" />
     </View>
   );
