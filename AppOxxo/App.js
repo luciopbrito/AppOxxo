152deade37e96599e54336939bf144bbd6b6e53a
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-03-23 12:01:56 -0300

    feat: tela-de-animacao: atualizado layout;

--- AppOxxo/App.js
+++ AppOxxo/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 import icon from './assets/oxxo_logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={icon}/>
-      <Text>Oxxo</Text>
-      <Text>Mercado</Text>
       <StatusBar style="auto" />
     </View>
   );
