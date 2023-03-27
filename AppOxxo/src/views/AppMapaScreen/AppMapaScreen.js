22196c45c3025cba7ceb70795aa9c367dd1ab471
Lúcio Brito <luciopereirabritoo@gmail.com>
2023-03-26 22:51:27 -0300

    feat: protipo_1: rotas feitas;

--- AppOxxo/src/views/AppMapaScreen/AppMapaScreen.js
+++ AppOxxo/src/views/AppMapaScreen/AppMapaScreen.js
@@ -1,65 +1,88 @@
+import { Feather } from "@expo/vector-icons";
+import { Avatar, Box, HStack, Icon, NativeBaseProvider, Text } from "native-base";
 import React from "react";
+import { Pressable } from "react-native";
 import { Image, StyleSheet, View } from "react-native";
 import MapView from 'react-native-maps';
+import Logo from '../../../assets/logo.png'
 
 export default function AppMapaScreen() {
 
     return (
-
-        <View style={Styles.container}>
-            <View style={Styles.boxLogo}>
-                <Image style={Styles.image_logo} source={require('../../../assets/logo.png')} />
-            </View>
-
-            <View>
-                <MapView style={Styles.map}
-                    initialRegion={{
-                        latitude: -23.464363,
-                        longitude: -46.523467,
-                        latitudeDelta: 0.00922,
-                        longitudeDelta: 0.00421,
-                    }}
-                >
-                </MapView>
-            </View>
-        </View>
-
+        <NativeBaseProvider>
+            <HStack background={'#f00'} marginTop='10' paddingTop={5} width="100%" h='auto' display={'flex'} alignItems="center" justifyContent="space-between">
+                <Box>
+                    <Pressable>
+                        <Icon
+                            as={Feather}
+                            name="menu"
+                            size={10}
+                            color="#000"
+                        >
+                        </Icon>
+                    </Pressable>
+                </Box>
+                <Box display='flex' alignItems="center">
+                    <Image source={Logo} alt='Image Logo Oxxo' />
+                </Box>
+                <Box>
+                    <Avatar source={{ uri: 'https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png' }} />
+                </Box>
+            </HStack>
+            <Box style={Styles.container} safeArea>
+                <View>
+                    <MapView style={Styles.map}
+                        initialRegion={{
+                            latitude: -23.464363,
+                            longitude: -46.523467,
+                            latitudeDelta: 0.00922,
+                            longitudeDelta: 0.00421,
+                        }}
+                    >
+                    </MapView>
+                </View>
+            </Box>
+            <Tabs></Tabs>
+        </NativeBaseProvider >
     )
 }
 
-
-//===== CSS =======///
-
-
+const Tabs = () => {
+    return (
+        <Box paddingTop='5' paddingBottom='5' height='10 % ' width='100 % ' display='flex' justifyContent='space-around' flexDirection='row' backgroundColor="#000">
+            <Box Box style={Styles.buttonScreen} >
+                <Icon
+                    as={Feather}
+                    name="shopping-cart"
+                    size={10}
+                    color="#000"
+                />
+            </Box>
+            <Box style={Styles.buttonScreen}>
+                <Icon
+                    as={Feather}
+                    name="map"
+                    size={10}
+                    color="#000"
+                >
+                </Icon>
+            </Box>
+        </Box >
+    )
+}
 
 const Styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
         backgroundColor: "#f00",
         justifyContent: 'center',
-        paddingHorizontal: 2,
     },
     map: {
         height: "90%",
-
-
     },
-    boxLogo: {
-        height: "15%",
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 30,
-
-
-    },
-    image_logo: {
-        borderRadius: 5,
-        height: '45%',
-        width: '20%'
-
-
-    },
-
-
-
+    buttonScreen: {
+        borderWidth: 1,
+        borderRadius: 5
+    }
 })
--- AppOxxo/src/views/ProdutosScreen/ProdutosScreen.js
+++ AppOxxo/src/views/ProdutosScreen/ProdutosScreen.js
@@ -1,47 +1,15 @@
 import React, { useEffect, useState } from "react"
 import { HStack, Pressable, Icon, Box, Text, NativeBaseProvider, Image, Avatar, FlatList } from "native-base"
 import { Feather } from "@expo/vector-icons"
-import { Animated, Button } from "react-native"
+import { Animated, Button, StyleSheet } from "react-native"
 import Logo from '../../../assets/logo.png'
 import LoginScreen from '../LoginScreen/LoginScreen'
+import { ScrollView } from "react-native-gesture-handler"
 
 export default function ProdutosScreen({ navigation }) {
     const [openMenu, setOpenMenu] = useState(false);
     const [animationMenu, setAnimationMenu] = useState(new Animated.Value(0));
     // const Drawer = createDrawerNavigator()
-
-    const data = [
-        {
-            id: 1,
-            name: 'Leite em pó 500g',
-            uri: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSxUvR_hVNAd83dnKmPN48cuqXVoqFMg5SVgvS4w7g1pMbYeRfHK9DSYaIXmZHNnF1xZS0W7Z8z20s&usqp=CAc',
-        },
-        {
-            id: 2,
-            name: 'café Pilão 400g',
-            uri: 'https://qualycestas.vteximg.com.br/arquivos/ids/157479-292-292/WhatsApp-Image-2022-07-14-at-07.27.57.jpg?v=637937623884600000',
-        },
-        {
-            id: 3,
-            name: 'Margaina 500g',
-            uri: 'https://naturaldaterra.com.br/media/catalog/product/1/2/123375---7893000383005---marg-qualy-ssal-500g.jpg?auto=webp&format=pjpg&width=640&height=800&fit=cover',
-        },
-        {
-            id: 4,
-            name: 'Pao de Forma',
-            uri: 'https://images.tcdn.com.br/img/img_prod/666230/pao_de_forma_visconti_400g_1765_1_20200514234408.jpg',
-        },
-        {
-            id: 5,
-            name: 'Leite Integral 1L',
-            uri: 'https://m.media-amazon.com/images/I/51wNnRI8zTL._AC_SX522_.jpg',
-        },
-        {
-            id: 6,
-            name: 'Biscoito 400g',
-            uri: 'https://a-static.mlcdn.com.br/800x560/biscoito-de-polvilho-salgado-globo-30g/casasantaluzia2/354/1e616f604cbeebd35b3e5f86b646a878.jpeg',
-        },
-    ]
 
     const [datas, setDatas] = useState([])
 
@@ -49,17 +17,10 @@ export default function ProdutosScreen({ navigation }) {
         setDatas(data);
     }, [])
 
-
-
     return (
-        // <Drawer.Navigator initialRouteName="Produtos">
-        //     <Drawer.Screen name="Login" component={LoginScreen} />
-        //     <Drawer.Screen name="Produtos" component={ProdutosScreen} />
-        // </Drawer.Navigator>
         <NativeBaseProvider>
-
-            <Box flex={1} backgroundColor="#f8f8f8" flexDirection="column">
-                <HStack padding={4} w="100%" alignItems="center" justifyContent="space-between" safeArea >
+            <Box flex={1} backgroundColor="#f8f8f8" flexDirection="column" safeArea>
+                <HStack background={'#f00'} paddingTop='15' paddingBottom='15' paddingRight='10' paddingLeft='10' w="100%" display={'flex'} alignItems="center" justifyContent="space-between"  >
                     <Box>
                         <Pressable>
                             <Icon
@@ -79,25 +40,89 @@ export default function ProdutosScreen({ navigation }) {
                         <Avatar source={{ uri: 'https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png' }} />
                     </Box>
                 </HStack>
-                <Box>
-                    <FlatList data={{ id: 1 }} horizontal={true} renderItem={({ item, index }) => {
-                        // <Produto item={item} />
-                        <Text key={index}>{item.id}</Text>
-                    }} />
-                </Box>
+                <ScrollView>
+                    <Box style={styles.container}>
+                        <Produto item={{
+                            id: 1,
+                            name: 'Leite em pó 500g',
+                            uri: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSxUvR_hVNAd83dnKmPN48cuqXVoqFMg5SVgvS4w7g1pMbYeRfHK9DSYaIXmZHNnF1xZS0W7Z8z20s&usqp=CAc',
+                        }} />
+                        <Produto item={{
+                            id: 2,
+                            name: 'café Pilão 400g',
+                            uri: 'https://qualycestas.vteximg.com.br/arquivos/ids/157479-292-292/WhatsApp-Image-2022-07-14-at-07.27.57.jpg?v=637937623884600000',
+                        }} />
+                        <Produto item={{
+                            id: 3,
+                            name: 'Margaina 500g',
+                            uri: 'https://naturaldaterra.com.br/media/catalog/product/1/2/123375---7893000383005---marg-qualy-ssal-500g.jpg?auto=webp&format=pjpg&width=640&height=800&fit=cover',
+                        }} />
+                        <Produto item={{
+                            id: 4,
+                            name: 'Pao de Forma',
+                            uri: 'https://images.tcdn.com.br/img/img_prod/666230/pao_de_forma_visconti_400g_1765_1_20200514234408.jpg',
+                        }} />
+                        <Produto item={{
+                            id: 5,
+                            name: 'Leite Integral 1L',
+                            uri: 'https://m.media-amazon.com/images/I/51wNnRI8zTL._AC_SX522_.jpg',
+                        }} />
+                        <Produto item={{
+                            id: 6,
+                            name: 'Biscoito 400g',
+                            uri: 'https://a-static.mlcdn.com.br/800x560/biscoito-de-polvilho-salgado-globo-30g/casasantaluzia2/354/1e616f604cbeebd35b3e5f86b646a878.jpeg',
+                        }} />
+
+                    </Box>
+                </ScrollView>
             </Box>
-            {openMenu ?
-                <Drawer.Navigator initialRouteName="Produtos">
-                    <Drawer.Screen name="Login" component={LoginScreen} />
-                    <Drawer.Screen name="Produtos" component={ProdutosScreen} />
-                </Drawer.Navigator>
-                :
-                null}
-        </NativeBaseProvider>
+            {
+                openMenu ?
+                    <Box>
+
+                    </Box>
+                    :
+                    null
+            }
+        </NativeBaseProvider >
     )
 }
 
-const Produto = (item) => {
+
+const data = [
+    {
+        id: 1,
+        name: 'Leite em pó 500g',
+        uri: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSxUvR_hVNAd83dnKmPN48cuqXVoqFMg5SVgvS4w7g1pMbYeRfHK9DSYaIXmZHNnF1xZS0W7Z8z20s&usqp=CAc',
+    },
+    {
+        id: 2,
+        name: 'café Pilão 400g',
+        uri: 'https://qualycestas.vteximg.com.br/arquivos/ids/157479-292-292/WhatsApp-Image-2022-07-14-at-07.27.57.jpg?v=637937623884600000',
+    },
+    {
+        id: 3,
+        name: 'Margaina 500g',
+        uri: 'https://naturaldaterra.com.br/media/catalog/product/1/2/123375---7893000383005---marg-qualy-ssal-500g.jpg?auto=webp&format=pjpg&width=640&height=800&fit=cover',
+    },
+    {
+        id: 4,
+        name: 'Pao de Forma',
+        uri: 'https://images.tcdn.com.br/img/img_prod/666230/pao_de_forma_visconti_400g_1765_1_20200514234408.jpg',
+    },
+    {
+        id: 5,
+        name: 'Leite Integral 1L',
+        uri: 'https://m.media-amazon.com/images/I/51wNnRI8zTL._AC_SX522_.jpg',
+    },
+    {
+        id: 6,
+        name: 'Biscoito 400g',
+        uri: 'https://a-static.mlcdn.com.br/800x560/biscoito-de-polvilho-salgado-globo-30g/casasantaluzia2/354/1e616f604cbeebd35b3e5f86b646a878.jpeg',
+    },
+]
+
+const Produto = ({ item }) => {
 
     const modalProduto = (id) => {
         var produto = data.find((e) => e.id == id);
@@ -106,16 +131,42 @@ const Produto = (item) => {
     }
 
     return (
-        <Box>
-            <Image source={{ uri: item.uri }} />
+        <Box style={styles.containerProduto}>
+            <Image source={{ uri: item.uri }} alt="image" size='xl' />
+            <Text style={styles.titleProduto}>{item.name}</Text>
             <Box>
-                <Text style={styles.titleProduto}>{item.name}</Text>
-                <Button title='Comprar' onPress={() => { modalProduto(item.id) }} />
+                <Button color='#FBB110' style={styles.buttonProduto} title='Comprar' onPress={() => { modalProduto(item.id) }} />
             </Box>
         </Box>
-
     )
 }
+
+const styles = StyleSheet.create({
+    titleProduto: {
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    container: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    containerProduto: {
+        backgroundColor: '#fff',
+        width: "45%",
+        borderRadius: 5,
+        padding: 10,
+        margin: 5,
+        borderWidth: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 10
+    },
+    buttonProduto: {
+    }
+})
 
 // onPress={() => {
 //     var hasOption = item.optionsScreen !== null ? item.optionsScreen : null
